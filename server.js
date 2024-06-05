afef30303dce2570c424db1061b5dc69d04ea770
codezcracker <36415183+codezcracker@users.noreply.github.com>
2024-06-06 03:19:52 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,22 @@
 const express = require('express');
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
-const { Server } = require('socket.io');
+const app = express();
+const port = 3000;
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// Middleware to parse JSON bodies
+app.use(express.json());
 
-const PORT = process.env.PORT || 4001;
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.prepare().then(() => {
-	const server = express();
-	const httpServer = createServer(server);
-	const io = new Server(httpServer, { cors: { origin: '*' } });
-	
-	let buttonState = false;
-	
-	io.on('connection', (socket) => {
-		console.log('New Connection');
-		
-		io.to(socket.id).emit('buttonState', buttonState);
-		
-		socket.on('disconnect', () => {
-			console.log('Disconnected');
-		});
-		
-		socket.on('buttonState', (value) => {
-			console.log('buttonState:', value);
-			buttonState = value;
-			socket.broadcast.emit('buttonState', value);
-		});
-	});
-	
-	server.all('*', (req, res) => {
-		const parsedUrl = parse(req.url, true);
-		handle(req, res, parsedUrl);
-	});
-	
-	httpServer.listen(PORT, (err) => {
-		if (err) throw err;
-		console.log(`Server running on http://localhost:${PORT}`);
-	});
+// POST route to handle incoming data
+app.post('/data', (req, res) => {
+  const { value } = req.body;
+  console.log('Received value:', value);
+  res.send(`Received value: ${value}`);
+});
+
+app.listen(port, () => {
+  console.log(`API listening at http://localhost:${port}`);
 });
