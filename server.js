4bc48994a802f27cc86a94954c9ef02c7b681747
codezcracker <36415183+codezcracker@users.noreply.github.com>
2024-06-06 00:08:26 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,29 @@
 const express = require('express');
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
-const { Server } = require('socket.io');
+const cors = require('cors');
+const app = express();
+const port = 4001;
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const corsOptions = {
+	origin: 'https://esp-app-latest.vercel.app',
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	credentials: true,
+	allowedHeaders: 'Content-Type,Authorization',
+	exposedHeaders: 'Content-Length,Content-Range',
+	preflightContinue: false,
+	optionsSuccessStatus: 204,
+};
 
-const PORT = process.env.PORT || 4001;
+app.use(cors(corsOptions));
 
-app.prepare().then(() => {
-	const server = express();
-	const httpServer = createServer(server);
-	const io = new Server(httpServer, { cors: { origin: '*' } });
-	
-	let buttonState = false;
-	
-	io.on('connection', (socket) => {
-		console.log('New Connection');
-		
-		io.to(socket.id).emit('buttonState', buttonState);
-		
-		socket.on('disconnect', () => {
-			console.log('Disconnected');
-		});
-		
-		socket.on('buttonState', (value) => {
-			console.log('buttonState:', value);
-			buttonState = value;
-			socket.broadcast.emit('buttonState', value);
-		});
-	});
-	
-	server.all('*', (req, res) => {
-		const parsedUrl = parse(req.url, true);
-		handle(req, res, parsedUrl);
-	});
-	
-	httpServer.listen(PORT, (err) => {
-		if (err) throw err;
-		console.log(`Server running on http://localhost:${PORT}`);
-	});
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Private-Network', 'true');
+	next();
+});
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}/`);
 });
